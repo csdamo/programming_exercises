abde3de5708cd2b6453681424760cd4682ca81eb
csdamo <cristiane@classcode.com.br>
2022-08-07 15:29:46 -0300

    add fila_base

--- 3_python/3_6_python_boas_praticas/fila_prioritario.py
+++ 3_python/3_6_python_boas_praticas/fila_prioritario.py
@@ -1,31 +1,21 @@
-from errno import ESTALE
+from fila_base import FilaBase
 
 
-class FilaPrioridade:
-    codigo: int = 0
-    fila = []
-    clientes_atendidos = []
-    senha_atual: str = ''
-    
+class FilaPrioridade(FilaBase):
+
     def gera_senha_atual(self) -> None:
         self.senha_atual = f'PR {self.codigo}'
-        
-    def reseta_fila(self) -> None:
-        if self.codigo >= 100:
-            self.codigo = 0
-        else:
-            self.codigo += 1
-            
+
     def atualiza_fila(self) -> None:
         self.reseta_fila()
         self.gera_senha_atual()
         self.fila.append(self.senha_atual)
-        
-    def chama_cliente(self, caixa:int) -> str:
-        cliente_atual:str = self.fila.pop(0)
+
+    def chama_cliente(self, caixa: int) -> str:
+        cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual} - Caixa: {caixa}'
-    
+
     def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
         if flag != 'detail':
             estatistica = {f'{agencia} - {dia}': len(self.clientes_atendidos)}
@@ -37,4 +27,3 @@ class FilaPrioridade:
                 'quantidade': len(self.clientes_atendidos)
             }
         return estatistica
-    
